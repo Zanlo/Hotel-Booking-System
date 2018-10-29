9b315ff450dc5168ab0fda79f6a20494042e0cf0
Uxmal <uxmal.c@gmail.com>
2018-10-30 06:54:39 +1100

    added files to be pulled

--- WebHotel/Program.cs
+++ WebHotel/Program.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using WebHotel.Data;
 
 namespace WebHotel
 {
@@ -14,7 +16,31 @@ namespace WebHotel
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            // create the WebHost object
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                // get the service providers
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var serviceProvider = services.GetRequiredService<IServiceProvider>();
+                    // get the Config object for the appsettings.json file 
+                    var configuration = services.GetRequiredService<IConfiguration>();
+                    // pass the service proiders and the config object to CreateRoles()
+                    SeedRoles.CreateRoles(serviceProvider, configuration).Wait();
+
+                }
+                catch (Exception exception)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(exception, "An error occurred while creating roles");
+                }
+            }
+
+            // start the web app
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
